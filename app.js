008f74b091718b8f9d1a6f1339fe5c3d9faa4d06
Alexandria Nichols <64329017+amnichols93@users.noreply.github.com>
2020-07-24 08:45:26 -0700

    challenge commit files

--- app.js
+++ app.js
@@ -23,15 +23,32 @@ function buildTable(data) {
 
 function handleClick() {
     let date = d3.select("#datetime").property("value");
+    let city = d3.select("#city").property("value");
+    let state = d3.select("#state").property("value");
+    let country = d3.select("#country").property("value");
+    let shape = d3.select("#shape").property("value");
     let filteredData = tableData;
     if (date) {
         filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    if (city) {
+        filteredData = filteredData.filter(row => row.city === city);
+    };
+    if (state) {
+        filteredData = filteredData.filter(row => row.state === state);
+    };
+    if (country) {
+        filteredData = filteredData.filter(row => row.country === country);
+    };
+    if (shape) {
+        filteredData = filteredData.filter(row => row.shape === shape);
     };
     buildTable(filteredData);
 }
 
 // Attach an event to listen for the form button
 d3.selectAll("#filter-btn").on("click", handleClick);
+d3.selectAll("#reset-btn").on("click", buildTable(tableData));
 
 // Build the table when the page loads
 buildTable(tableData);
